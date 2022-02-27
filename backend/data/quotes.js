ea0460951e5f9fbe2cc1f5b322827d46f7d7511a
tejaavula <teja.avula076@gmail.com>
2022-02-27 09:36:23 -0600

    Intialized out frontend and backend

---
+++ backend/data/quotes.js
@@ -0,0 +1,31 @@
+const quotes = [
+  {
+    _id: "1",
+    title: "Education",
+    quote:
+      "Education is the most powerful weapon which you can use to change the world",
+    category: "Edu",
+  },
+  {
+    _id: "2",
+    title: "Stress",
+    quote: "Stress should be a powerful driving force, not an obstacle.",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "Job",
+    quote:
+      "It's not what you achieve, it's what you overcome. That's what defines your career.",
+    category: "Inspiration",
+  },
+  {
+    _id: 4,
+    title: "Peacefull",
+    quote:
+      "Enjoy the little things, for one day you may look back and realize they were the big things.",
+    category: "Gratitude",
+  },
+];
+
+module.exports = quotes;
---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+import "./App.css";
+
+const App = () => <h1 className="App">QuoteMaker</h1>;
+
+export default App;
